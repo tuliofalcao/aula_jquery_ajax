cb4bf4581b2e4806aca35baf2303a329f05f326d
Túlio Falcão <falcao.tulio@gmail.com>
2020-06-30 22:51:56 -0300

    Final de aula

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 function consultaCep(){
+    $(".barra-progresso").show();
     var cep = document.getElementById("cep").value;
     var url = "http://viacep.com.br/ws/" + cep + "/json/";
     $.ajax({
@@ -7,11 +8,16 @@ function consultaCep(){
         success: function(response){
             console.log(response.logradouro);
             $("#logradouro").html(response.logradouro);
-            //document.getElementById("logradouro").innerHTML = response.logradouro;
-            document.getElementById("bairro").innerHTML = response.bairro;
-            document.getElementById("localidade").innerHTML = response.localidade;
-            document.getElementById("uf").innerHTML = response.uf;
-            
+            $("#bairro").html(response.bairro);
+            $("#localidade").html(response.localidade);
+            $("#uf").html(response.uf);
+            $("#titulo_cep").html("CEP " + response.cep);
+            $(".barra-progresso").hide();
+            $(".cep").show();                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
         }
     })
 }
+$(function(){
+    $(".cep").hide();
+    $(".barra-progresso").hide();
+})
